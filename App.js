fd092d7f08bf84284666286c713745ca60c1a5d4
Osnel Arriaga <osnelarriaga@gmail.com>
2020-03-09 23:21:14 -0700

    Fixing Bugs

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 // Esto es una alerta de pantalla en el inicio
-s
+
 var user = prompt('Porfavor ingresa tu nombre:');
 
 alert(user + ' ' + 'esta pagina esta en desarrollo constante, gracias por visitarla ;) ');
