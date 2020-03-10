713cc704a3a7107049addc522ee87ecee9d1f68c
Osnel Arriaga <osnelarriaga@gmail.com>
2020-03-09 20:32:33 -0700

    Añadido lenguaje JavaScript

---
+++ App.js
@@ -0,0 +1,6 @@
+// Esto es una alerta de pantalla en el inicio
+
+var user = prompt('Porfavor ingresa tu nombre:');
+
+alert(user + ' ' + 'esta pagina esta en desarrollo constante, gracias por visitarla ;) ');
+
